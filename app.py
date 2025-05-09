655169a44a8efc6a7c7d40c0b9a05214ce1dc889
ScottK156 <scottk8@nycstudents.net>
2025-05-09 09:23:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ import requests
 
 app =Flask(__name__)
 @app.route("/")
+
 def index():
-    
+    respsonse = requests.get("https://api.chess.com/pub/player/stats")
+    data = respsonse.json()
+    print(data)
+index()
