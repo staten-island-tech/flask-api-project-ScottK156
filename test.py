e82e071711d7eaef7d1a7264278795fe22a1e590
ScottK156 <scottk8@nycstudents.net>
2025-05-14 09:24:25 -0400

    work

--- test.py
+++ test.py
@@ -1,5 +1,28 @@
-from flask import Flask, render_template
+""" import requests
+
+username = "magnuscarlsen"
+url = f"https://api.chess.com/pub/player/{username}/stats"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print(f"Error: Failed to fetch data (status code {response.status_code})")  """
 import requests
 
-app =Flask(__name__)
-@app.route("/")
+username = "magnuscarlsen"
+url = f"https://api.chess.com/pub/player/{username}/stats"
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+}
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print(f"Error: Failed to fetch data (status code {response.status_code})")
